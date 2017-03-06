b5d048c13901b994b06d8c181238ae7cbd874464
Simon Tanguay <simontanguay@hotmail.com>
2017-03-05 19:05:22 -0500

    add MsgResponse.cs

---
+++ Robot/DigitalIOControl/MsgResponse.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: admin
+ * Date: 3/5/2017
+ * Time: 1:47 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using MccDaq;
+
+namespace DigitalIOControl
+{
+	/// <summary>
+	/// Description of MsgResponse.
+	/// </summary>
+	[Serializable]
+	public class MsgResponse : MessageType
+	{
+		public ErrorInfo.ErrorCode ErrorCode {get; set; }	
+		public MessageType Message {get; set; }	
+		
+		public MsgResponse()
+		{
+			
+		}
+	}
+}
